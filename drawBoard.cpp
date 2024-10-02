a7a865ed8741df27d4bd158e5cbbc3b894f067f0
Dinu Bricicari <dinu.bricicari@mail.ru>
2024-10-02 22:39:43 +0300

    Cpps

---
+++ drawBoard.cpp
@@ -0,0 +1,18 @@
+// drawBoard.cpp
+#include "drawBoard.hpp"
+#include <iostream>
+using namespace std;
+
+void drawBoard(char* spaces) {
+    cout << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[0] << "  |  " << spaces[1] << "  |  " << spaces[2] << "  " << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[3] << "  |  " << spaces[4] << "  |  " << spaces[5] << "  " << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[6] << "  |  " << spaces[7] << "  |  " << spaces[8] << "  " << '\n';
+    cout << "     |     |     " << '\n';
+    cout << '\n';
+}
